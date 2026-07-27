cc5b7ccf0ab61093198b95de3ac376fd08cc2a48
jenssd <jens.eidinger@fecher.eu>
2026-07-27 08:39:05 +0200

    Add generated item database placeholder

---
+++ Addons/PrivateWoWAdmin/PrivateWoWAdminItemData.lua
@@ -0,0 +1,5 @@
+PrivateWoWAdminItems = PrivateWoWAdminItems or {}
+PrivateWoWAdminItems.data = PrivateWoWAdminItems.data or {}
+
+-- Diese Datei wird durch scripts/generate-item-database.ps1 erzeugt.
+-- Format: PrivateWoWAdminItems.data[ITEM_ID] = "ITEM_NAME"

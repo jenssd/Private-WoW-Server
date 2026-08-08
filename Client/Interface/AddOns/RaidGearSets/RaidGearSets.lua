92e35637be4c4ef0b89547c56091272c25b315ee
jenssd <jens.eidinger@fecher.eu>
2026-08-08 17:44:33 +0200

    Add glyphs and enchant scrolls to Shaman gear sets

--- Client/Interface/AddOns/RaidGearSets/RaidGearSets.lua
+++ Client/Interface/AddOns/RaidGearSets/RaidGearSets.lua
@@ -7,61 +7,99 @@ local sets = {
     gear60ele = {
         name = "Schamane 60 Elementar",
         commands = {
+            -- Gear
             ".additem 19375", ".additem 21608", ".additem 23664", ".additem 23050",
             ".additem 21838", ".additem 21186", ".additem 21585", ".additem 22730",
             ".additem 23070", ".additem 21600", ".additem 21709", ".additem 23031",
             ".additem 19379", ".additem 23046", ".additem 22988", ".additem 23049",
-            ".additem 23199"
+            ".additem 23199",
+            -- Glyphs: Lightning Bolt, Totem of Wrath; Water Shield, Renewed Life
+            ".additem 41536", ".additem 45776", ".additem 43386", ".additem 43385",
+            -- Enchant scrolls: cloak haste, chest stats, bracer SP, glove hit, boots hit/crit, weapon SP
+            ".additem 39003", ".additem 44465", ".additem 44470", ".additem 38953",
+            ".additem 38986", ".additem 44467"
         }
     },
     gear60enh = {
         name = "Schamane 60 Verstaerker",
         commands = {
+            -- Gear and gems
             ".additem 22478", ".additem 19377", ".additem 21665", ".additem 21710",
             ".additem 21680", ".additem 22483", ".additem 18823", ".additem 24063",
             ".additem 24022", ".additem 21612", ".additem 19432", ".additem 22961",
             ".additem 22954", ".additem 23041", ".additem 23221", ".additem 22808",
-            ".additem 40118 2", ".additem 40125 3"
+            ".additem 40118 2", ".additem 40125 3",
+            -- Glyphs: Stormstrike, Feral Spirit; Water Shield, Renewed Life
+            ".additem 41539", ".additem 45771", ".additem 43386", ".additem 43385",
+            -- Enchant scrolls: cloak haste, chest stats, bracer expertise, glove hit, boots hit/crit, Berserking x2
+            ".additem 39003", ".additem 44465", ".additem 38984", ".additem 38953",
+            ".additem 38986", ".additem 44493 2"
         }
     },
     gear70ele = {
         name = "Schamane 70 Elementar",
         commands = {
+            -- Gear and gems
             ".additem 34332", ".additem 34204", ".additem 31023", ".additem 34242",
             ".additem 34364", ".additem 34437", ".additem 34344", ".additem 34542",
             ".additem 34186", ".additem 34566", ".additem 34362", ".additem 34230",
             ".additem 32483", ".additem 34429", ".additem 34336", ".additem 34179",
-            ".additem 32330", ".additem 40125 4"
+            ".additem 32330", ".additem 40125 4",
+            -- Glyphs: Lightning Bolt, Lava; Water Shield, Renewed Life, Water Walking
+            ".additem 41536", ".additem 41524", ".additem 43386", ".additem 43385", ".additem 43388",
+            -- Enchant scrolls: cloak haste, chest stats, bracer SP, gloves SP, movement boots, weapon SP
+            ".additem 39003", ".additem 44465", ".additem 44470", ".additem 38979",
+            ".additem 39006", ".additem 44467"
         }
     },
     gear70enh = {
         name = "Schamane 70 Verstaerker",
         commands = {
+            -- Gear and gems
             ".additem 34244", ".additem 34358", ".additem 34392", ".additem 34241",
             ".additem 34397", ".additem 34439", ".additem 34343", ".additem 34545",
             ".additem 34188", ".additem 34567", ".additem 34189", ".additem 32497",
             ".additem 34427", ".additem 34472", ".additem 34331", ".additem 34346",
-            ".additem 40125 6"
+            ".additem 40125 6",
+            -- Glyphs: Stormstrike, Feral Spirit; Water Shield, Renewed Life, Water Walking
+            ".additem 41539", ".additem 45771", ".additem 43386", ".additem 43385", ".additem 43388",
+            -- Enchant scrolls: cloak haste, chest stats, bracer AP, gloves AP, movement boots, Berserking x2
+            ".additem 39003", ".additem 44465", ".additem 44815", ".additem 44458",
+            ".additem 39006", ".additem 44493 2"
         }
     },
     gear80ele = {
         name = "Schamane 80 Elementar",
         commands = {
+            -- Gear and gems
             ".additem 51237", ".additem 50658", ".additem 50698", ".additem 54583",
             ".additem 51239", ".additem 54582", ".additem 51238", ".additem 54587",
             ".additem 51236", ".additem 50699", ".additem 50664", ".additem 50398",
             ".additem 50348", ".additem 50365", ".additem 50734", ".additem 50616",
-            ".additem 50458", ".additem 40125 3"
+            ".additem 50458", ".additem 40125 3",
+            -- Glyphs: Lightning Bolt, Lava, Totem of Wrath; Water Shield, Renewed Life, Water Walking
+            ".additem 41536", ".additem 41524", ".additem 45776",
+            ".additem 43386", ".additem 43385", ".additem 43388",
+            -- Enchant scrolls: cloak haste, chest stats, bracer SP, gloves SP, boots hit/crit, weapon SP, shield Int
+            ".additem 39003", ".additem 44465", ".additem 44470", ".additem 38979",
+            ".additem 38986", ".additem 44467", ".additem 44455"
         }
     },
     gear80enh = {
         name = "Schamane 80 Verstaerker",
         commands = {
+            -- Gear and gems
             ".additem 51242", ".additem 50633", ".additem 51240", ".additem 54583",
             ".additem 50656", ".additem 54580", ".additem 51243", ".additem 54587",
             ".additem 51241", ".additem 50711", ".additem 50604", ".additem 50402",
             ".additem 54588", ".additem 50365", ".additem 50734", ".additem 50737",
-            ".additem 50458", ".additem 40118", ".additem 40148"
+            ".additem 50458", ".additem 40118", ".additem 40148",
+            -- Spellhance glyphs: Stormstrike, Fire Nova, Flametongue Weapon; Water Shield, Renewed Life, Water Walking
+            ".additem 41539", ".additem 41530", ".additem 41532",
+            ".additem 43386", ".additem 43385", ".additem 43388",
+            -- Enchant scrolls: cloak haste, chest stats, bracer expertise, gloves AP, movement boots, Berserking x2
+            ".additem 39003", ".additem 44465", ".additem 38984", ".additem 44458",
+            ".additem 39006", ".additem 44493 2"
         }
     }
 }
@@ -109,7 +147,7 @@ local function StartSet(key)
     elapsed = 0
     runningName = set.name
 
-    Print("Starte " .. set.name .. " (" .. #queue .. " GM-Befehle).")
+    Print("Starte " .. set.name .. " (" .. #queue .. " GM-Befehle inkl. Gear/Gems/Glyphen/Verzauberungsrollen).")
 
     frame:SetScript("OnUpdate", function(self, delta)
         elapsed = elapsed + delta
@@ -122,7 +160,7 @@ local function StartSet(key)
         if not command then
             local completed = runningName
             StopQueue(true)
-            Print(completed .. " abgeschlossen.")
+            Print(completed .. " abgeschlossen. Glyphen und Verzauberungsrollen liegen im Inventar und muessen manuell angewendet werden.")
             return
         end
 
@@ -132,7 +170,7 @@ local function StartSet(key)
 end
 
 local function ShowHelp()
-    Print("Verfuegbare Befehle:")
+    Print("Verfuegbare Befehle (Gear + Gems + Glyphen + Verzauberungsrollen):")
     Print("/gear60ele - Schamane Level 60 Elementar")
     Print("/gear60enh - Schamane Level 60 Verstaerker")
     Print("/gear70ele - Schamane Level 70 Elementar")
